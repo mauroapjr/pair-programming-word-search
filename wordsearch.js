7f383c0a1668d98f33e00dbbe0791f6da62282f1
mauroapjr <mauroapjr@outlook.com>
2023-01-19 15:35:02 -0500

    finished challenge

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,35 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const printMatrix = (matrix) => {
+  for (const row of matrix) {
+    for (const el of row) {
+      process.stdout.write(el + " ");
     }
-}
+    process.stdout.write("\n");
+  }
+};
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  const longstring = letters
+    .map((letter) => {
+      return letter.join("");
+    })
+    .join("");
+  return longstring.includes(word);
+};
+
+const transpose = function (matrix) {
+  const newArray = [];
+  for (let i = 0; i < matrix[0].length; i++) {
+    newArray.push([]);
+  }
+
+  for (let x = 0; x < matrix.length; x++) {
+    for (let y = 0; y < matrix[x].length; y++) {
+      newArray[y].push(matrix[x][y]);
+    }
+  }
+  return newArray;
+};
+
+module.exports = wordSearch;
+
+// Denny Doan, Elisabeth Clarke, Mauro Alves
